0f7a1cd8703077a088fe142a35b13b8af27300bd
Rachael <rachaeloyelowo@icloud.com>
2024-01-22 22:56:36 -0500

    Added 2-AddTwoNumbers.py

--- 1-TwoSum.py
+++ 1-TwoSum.py
@@ -1,47 +1,14 @@
-# class Solution(object):
-    
-        def lengthTwo():
-            if (nums[0] + nums[1] == target):
-                return 0, 1
-        
-        def lengthThree():
-            lengthTwo()
-            if (nums[1] + nums[2] == target):
-                return 1, 2
-            elif (nums[0] + nums[2] == target):
-                return 0, 2
-        
-        def lengthFour():
-            lengthThree()
-            if (nums[0] + nums[3] == target):
-                return 0, 3
-            elif (nums[1] + nums[3] == target):
-                return 1, 3
-            elif (nums[2] + nums[3] == target):
-                return 2, 3
+class Solution(object):
+    def twoSum(self, nums, target):
 
-        if (len(nums) == 2):
-            if (nums[0] + nums[1] == target):
-                return 0, 1
-        elif (len(nums) == 3):
-            if (nums[0] + nums[1] == target):
-                return 0, 1
-            elif (nums[1] + nums[2] == target):
-                return 1, 2
-            elif (nums[0] + nums[2] == target):
-                return 0, 2
-        elif (len(nums) == 4):
-            if (nums[0] + nums[1] == target):
-                return 0, 1
-            elif (nums[1] + nums[2] == target):
-                return 1, 2
-            elif (nums[0] + nums[2] == target):
-                return 0, 2
-            elif (nums[0] + nums[3] == target):
-                return 0, 3
-            elif (nums[1] + nums[3] == target):
-                return 1, 3
-            elif (nums[2] + nums[3] == target):
-                return 2, 3
-        else:
-            return 0, 0
+        hashMap = {}  # val : index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            
+            hashMap[n] = i
+        
+        return
