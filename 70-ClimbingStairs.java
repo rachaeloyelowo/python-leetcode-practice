5c0106c7fca4fa7291686f1fa5c0e2117ebb34a7
rachaeloyelowo <rachaeloyelowo123@gmail.com>
2025-01-02 17:30:52 -0500

    Added Problem 70 Solution

--- 70-ClimbingStairs.java
+++ 70-ClimbingStairs.java
@@ -1,19 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-
-        if (n == 0){
-            return 1;
+        int p1 = 1; //points to the second to last step
+        int p2 = 1; //points to the last step
+        for (int i = 0; i < n - 1; i++){
+            int temp = p1; 
+            p1 += p2; //for each step, p1 and p2 are added together and stored in p1
+            p2 = temp; //for each step p2; shifts to become the former p1
         }
-
-        int stairs = 0;
-        if (n - 1 >= 0){
-            stairs += climbStairs(n-1);
-        }
-
-        if (n - 2 >= 0){
-            stairs += climbStairs(n-2);
-        }
-
-        return stairs;
+        return p1;
     }
 }
